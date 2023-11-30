89193928cb642ca898da3b54be3aa90b9dc0390a
ParkJuHan94 <1004sunye@naver.com>
2023-12-01 00:50:54 +0900

    feat: gmarket 크롤링 최초 분기점

--- src/main/python/네이버_로그인_자동화.py
+++ src/main/python/네이버_로그인_자동화.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.keys import Keys
 
 # 브라우저 꺼짐 방지
 chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)43
+chrome_options.add_experimental_option("detach", True)
 
 # 크롬 드라이버 자동 업데이트
 from webdriver_manager.chrome import ChromeDriverManager
