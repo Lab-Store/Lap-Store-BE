961beefaaa594d421f11bbcb9061e2c488436aee
ParkJuHan94 <1004sunye@naver.com>
2024-01-20 21:10:55 +0900

    feat: main application 정리

---
+++ src/main/java/com/project/lapstore/LapStoreApplication.java
@@ -0,0 +1,14 @@
+package com.project.lapstore;
+
+import java.io.IOException;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LapStoreApplication {
+
+	public static void main(String[] args) throws IOException {
+		// TODO 추후 구현하기
+
+	}
+}
