5861290d718b8dde18700e0f074830fd196bce4b
ParkJuHan94 <1004sunye@naver.com>
2024-01-20 21:03:29 +0900

    feat: 필요없는 파일 삭제2

--- src/main/java/com/project/LapStore/repository/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.LapStore.repository;
-
-import com.project.LapStore.domain.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-
-}
