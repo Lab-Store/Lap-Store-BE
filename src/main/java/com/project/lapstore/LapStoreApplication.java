a56bbfc5435f6963d7e5d9c5114320db58a90ded
ParkJuHan94 <1004sunye@naver.com>
2024-01-31 21:50:45 +0900

    feat: item 엔티티 wishCount 컬럼 에러 수정

--- src/main/java/com/project/lapstore/LapStoreApplication.java
+++ src/main/java/com/project/lapstore/LapStoreApplication.java
@@ -1,14 +1,12 @@
 package com.project.lapstore;
 
-import java.io.IOException;
-
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LapStoreApplication {
 
-	public static void main(String[] args) throws IOException {
-		// TODO 추후 구현하기
-
+	public static void main(String[] args) {
+		SpringApplication.run(LapStoreApplication.class, args);
 	}
 }
