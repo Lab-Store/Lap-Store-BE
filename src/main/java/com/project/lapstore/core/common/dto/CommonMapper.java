646b2eba09d1903a4befe837c961be92a13b4334
ParkJuHan94 <1004sunye@naver.com>
2024-03-10 19:40:34 +0900

    [feat] : 공통 PageResponse 추가

---
+++ src/main/java/com/project/lapstore/core/common/dto/CommonMapper.java
@@ -0,0 +1,15 @@
+package com.project.lapstore.core.common.dto;
+
+import static lombok.AccessLevel.*;
+
+import org.springframework.data.domain.Slice;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = PRIVATE)
+public class CommonMapper {
+
+	public static <T> PageResponse<T> toPageResponse(Slice<T> page) {
+		return PageResponse.of(page);
+	}
+}
---
+++ src/main/java/com/project/lapstore/core/common/dto/PageResponse.java
@@ -0,0 +1,19 @@
+package com.project.lapstore.core.common.dto;
+
+import java.util.List;
+
+import org.springframework.data.domain.Slice;
+
+public record PageResponse<T>(
+	List<T> content,
+	long size,
+	boolean hasNext
+) {
+	public static <T> PageResponse<T> of(Slice<T> page) {
+		return new PageResponse<>(
+			page.getContent(),
+			page.getNumberOfElements(),
+			page.hasNext()
+		);
+	}
+}
